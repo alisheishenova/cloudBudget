1e68f7928302154d2d89f9626343850ab3fcfdad
sezim12 <vip.ssosezim825@gmail.com>
2021-07-13 11:57:42 +0600

    1 commit

---
+++ src/img/lids/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".carousel").owlCarousel({
+            items:1,
+            loop: true,
+            autoplay: true
+        // autoplayTimeout: 600,
+        // autoplayHoverPause: true
+        // nav: true
+    });
+});
